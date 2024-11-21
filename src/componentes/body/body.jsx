70b43e279e36933bd081c6f833dc033dd1ee4224
Maxisori <mj.soria@alumno.etec.um.edu.ar>
2024-11-20 21:45:02 -0300

    carrito listo

--- src/componentes/body/body.jsx
+++ src/componentes/body/body.jsx
@@ -63,7 +63,7 @@ export default function Body() {
 
         <div className="producto">
           <img className="imagen-producto" src={producto2Img} alt="Sueter1" />
-          <h2 className="nombreproducto">Remera Negra</h2>
+          <h2 className="nombreproducto">Remera Simple</h2>
           <p className="precio">Precio: 10.000</p>
           <button className="boton-carrito" onClick={() => addToCart({ name: "Sueter", price: 20000, img: producto1Img })}>
             Agregar
